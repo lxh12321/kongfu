bef253982a0e4d475065003fa98c22641d8492bb
lxh12321 <1183648121@qq.com>
2019-12-05 16:31:41 +0800

    qq

--- src/kongfu/xianglong.java
+++ src/kongfu/xianglong.java
@@ -5,5 +5,12 @@ public class xianglong {
 		// TODO Auto-generated method stub
 	System.out.println("11111");
 	System.out.println("22222");
+	System.out.println("33333");
+	System.out.println("44444");
+	System.out.println("55555");
+	System.out.println("66666");
+	System.out.println("77777");
+	System.out.println("88888");
+	System.out.println("99999");
 	}
 }
